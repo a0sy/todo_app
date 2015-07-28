eea4aa06e62717d49a7b74880a0dcf93f1928d3d
a0sy <a0sy.gl@gmail.com>
2015-07-28 16:55:41 +0900

    webpackの動作確認

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 /* require */
-//var Vue = require('vue');
+var html = require('./todo.html')
+document.getElementById("todo-wrapper").innerHTML = html;
+
+require('../css/style.css');
+require('../css/todo.css');
+
+var Vue = require('vue');
 
 var vm = new Vue({
   el: '#el',
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './js/todo.js',
+  output: {
+    path: __dirname,
+    filename: './js/bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.html$/, loader: 'html?minimize' },
+      { test: /\.css$/, loader: "style!css" }
+    ]
+  }
+};
