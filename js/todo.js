de27a4b918cfbcda4171bd9f3a3bb7219f0f8ba4
a0sy <a0sy.gl@gmail.com>
2015-07-30 17:44:16 +0900

    全て完了済みの場合はCheckAllボタンで全て未完了にする

--- js/todo.js
+++ js/todo.js
@@ -14,12 +14,28 @@ var vm = new Vue({
   el: '#todos',
   data: {
       inputTodoName: "",
-      todos: []
+      todos: [],
+      allCompleted: false
   },
   methods: {
       addTodo: function(){
         this.todos.push( { completed: false, name: this.inputTodoName } );
         this.inputTodoName = "";
+      },
+      allComplete: function() {
+        /* 全て完了済みかチェック */
+        this.allCompleted = this.todos.every(function(element){ return (element.completed === true) });
+
+        /* 全て完了済みだった場合, 全て未完了にする / 未完了がある場合は全て完了にする */
+        if (this.allCompleted === true) {
+          for (i = 0; i < this.todos.length; i++) {
+            this.todos[i].completed = false;
+          }
+        } else {
+          for (i = 0; i < this.todos.length; i++) {
+            this.todos[i].completed = true;
+          }
+        }
       }
   }
 });
